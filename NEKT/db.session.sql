b8f2073ba5ea2e59424252a8f5fd9c9aedc3d311
Alex  Paduret <72890769+p1xlized@users.noreply.github.com>
2024-10-06 13:23:56 -0400

    Delete NEKT directory

--- NEKT/db.session.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE Users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email TEXT,
-    age INTEGER,
-)
--- NEKT/db/db.js
+++
@@ -1,9 +0,0 @@
-const knex = require('knex');
-const db = knex ({
-    client : 'sqlite3',
-    connection: {
-        filename : './db/db.sqlite3'
-    },
-    useNullAsDefault : true
-}) 
-module.exports = db;
